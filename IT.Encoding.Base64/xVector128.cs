29f8d3b9ffca0d096e392798ffe810030e29fa7c
Ivan Tikhonov <itikhonov@mail.com>
2024-06-11 20:56:12 +0300

    refact

--- IT.Encoding.Base64/xVector128.cs
+++ IT.Encoding.Base64/xVector128.cs
@@ -17,13 +17,7 @@ internal static class xVector128
     public static void StoreUnsafe(Vector128<sbyte> vector, ref char destination)
     {
         ref short ptr = ref Unsafe.As<char, short>(ref destination);
-        Vector128.WidenLower(vector).AsInt16().StoreUnsafe(ref ptr);
-        Vector128.WidenUpper(vector).AsInt16().StoreUnsafe(ref ptr, 8);
-
-        /* Original
-                (Vector128<short> lower, Vector128<short> upper) = Vector128.Widen(v);
-                lower.StoreUnsafe(ref ptr);
-                upper.StoreUnsafe(ref ptr, 8);
-         */
+        Vector128.WidenLower(vector).StoreUnsafe(ref ptr);
+        Vector128.WidenUpper(vector).StoreUnsafe(ref ptr, 8);
     }
 }
